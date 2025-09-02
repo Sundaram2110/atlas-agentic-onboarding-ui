b22a64c3a41f036c10780a7fe9e97f84cd50fe7d
Sundaram2110 <mr.sundaram.2110@gmail.com>
2025-09-02 23:23:21 +0530

    Fetch agent is working just integrated , need to implement functionality

--- atlas-agentic-onboarding/atlas-ui/src/api/services/agent.ts
+++ atlas-agentic-onboarding/atlas-ui/src/api/services/agent.ts
@@ -1,9 +1,6 @@
 import api from '../axios';
 
-// export const chatAgent = async (message: string) => {
-//   const res = await api.post('/agent/chat', { message });
-//   return res.data;
-// };
-export async function chatAgent(request: RequestInfo, options?: RequestInit): Promise<any> {
-  return fetch(request, options).then(res => res.json());
-}
+export const chatAgent = async (message: string) => {
+  const res = await api.post('/agent/chat', { message });
+  return res.data;
+};
